1543997aeedb44986fc9581fb3aaa8b39152f095
Raman Dutt <s2198939@daisy1.inf.ed.ac.uk>
2025-04-21 03:40:24 +0100

    making training work

--- scripts/run_1024_finetune.sh
+++ scripts/run_1024_finetune.sh
@@ -18,8 +18,32 @@ NPROC_PER_NODE=4
 MASTER_PORT=1234 #1234
 NODE_RANK=0
 
-python -u finetune.py \
-    --master_port 18182 \
+# python -u finetune.py \
+#     --master_port 18182 \
+#     --global_bsz_${size} 128 \
+#     --micro_bsz_${size} 16 \
+#     --model ${model} \
+#     --lr ${lr} --grad_clip 2.0 \
+#     --data_path ${train_data_path} \
+#     --results_dir results/"$exp_name" \
+#     --data_parallel sdp \
+#     --max_steps 3000000 \
+#     --ckpt_every 1000 --log_every 10 \
+#     --precision ${precision} --grad_precision fp32 --qk_norm \
+#     --global_seed 20241207 \
+#     --num_workers 12 \
+#     --cache_data_on_disk \
+#     --snr_type ${snr_type} \
+#     --checkpointing \
+#     2>&1 | tee -a results/"$exp_name"/output.log
+#     # --init_from ${check_path} \
+
+torchrun \
+    --standalone \
+    --nnodes=$NNODES \
+    --nproc_per_node=$NPROC_PER_NODE \
+    finetune.py \
+    --master_port $MASTER_PORT \
     --global_bsz_${size} 128 \
     --micro_bsz_${size} 16 \
     --model ${model} \
@@ -33,8 +57,3 @@ python -u finetune.py \
     --global_seed 20241207 \
     --num_workers 12 \
     --cache_data_on_disk \
-    --snr_type ${snr_type} \
-    --checkpointing \
-    2>&1 | tee -a results/"$exp_name"/output.log
-
-    # --init_from ${check_path} \
