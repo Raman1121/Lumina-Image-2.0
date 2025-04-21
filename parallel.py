6865dfe1a8dc2417388dd0e3d284398c5b2bfe7e
Raman Dutt <s2198939@daisy1.inf.ed.ac.uk>
2025-04-21 04:04:02 +0100

    making training work

--- parallel.py
+++ parallel.py
@@ -9,22 +9,51 @@ import torch
 import torch.distributed as dist
 
 
-def _setup_dist_env_from_slurm(args):
-    # while not os.environ.get("MASTER_ADDR", ""):
-    #     os.environ["MASTER_ADDR"] = (
-    #         subprocess.check_output(
-    #             "sinfo -Nh -n %s | head -n 1 | awk '{print $1}'" % os.environ["SLURM_NODELIST"],
-    #             shell=True,
-    #         )
-    #         .decode()
-    #         .strip()
-    #     )
-    #     sleep(1)
-    os.environ["MASTER_PORT"] = str(args.master_port)
-    os.environ["RANK"] = os.environ["SLURM_PROCID"]
-    os.environ["WORLD_SIZE"] = os.environ["SLURM_NPROCS"]
-    os.environ["LOCAL_RANK"] = os.environ["SLURM_LOCALID"]
-    os.environ["LOCAL_WORLD_SIZE"] = os.environ["SLURM_NTASKS_PER_NODE"]
+# def _setup_dist_env_from_slurm(args):
+#     while not os.environ.get("MASTER_ADDR", ""):
+#         os.environ["MASTER_ADDR"] = (
+#             subprocess.check_output(
+#                 "sinfo -Nh -n %s | head -n 1 | awk '{print $1}'" % os.environ["SLURM_NODELIST"],
+#                 shell=True,
+#             )
+#             .decode()
+#             .strip()
+#         )
+#         sleep(1)
+#     os.environ["MASTER_PORT"] = str(args.master_port)
+#     os.environ["RANK"] = os.environ["SLURM_PROCID"]
+#     # os.environ["WORLD_SIZE"] = os.environ["SLURM_NPROCS"]
+#     os.environ["WORLD_SIZE"] = 4
+#     os.environ["LOCAL_RANK"] = os.environ["SLURM_LOCALID"]
+#     # os.environ["LOCAL_WORLD_SIZE"] = os.environ["SLURM_NTASKS_PER_NODE"]
+#     os.environ["LOCAL_WORLD_SIZE"] = 4
+
+def _setup_dist_env_from_launcher(args):
+    """
+    Sets up distributed environment, relying primarily on variables
+    set by a launcher like torchrun. Sets a default MASTER_PORT if needed.
+    """
+    # torchrun sets MASTER_ADDR, RANK, WORLD_SIZE, LOCAL_RANK, LOCAL_WORLD_SIZE
+    # It might also set MASTER_PORT, but we can provide a default.
+
+    if "MASTER_ADDR" not in os.environ:
+        # If launcher didn't set it (unlikely for torchrun), default for single node
+        os.environ["MASTER_ADDR"] = "127.0.0.1"
+        print("WARNING: MASTER_ADDR not set by launcher, defaulting to 127.0.0.1")
+
+    if "MASTER_PORT" not in os.environ:
+        os.environ["MASTER_PORT"] = str(args.master_port)
+        print(f"INFO: MASTER_PORT not set by launcher, using args.master_port: {args.master_port}")
+    else:
+        # If launcher set it, respect that value
+        print(f"INFO: Using MASTER_PORT set by launcher: {os.environ['MASTER_PORT']}")
+
+    # Optional: Check if essential variables are set
+    required_vars = ["RANK", "WORLD_SIZE", "LOCAL_RANK", "LOCAL_WORLD_SIZE"]
+    missing_vars = [v for v in required_vars if v not in os.environ]
+    if missing_vars:
+        print(f"ERROR: Required environment variables not set by launcher: {missing_vars}")
+        print("       Ensure you are using a PyTorch distributed launcher like torchrun.")
 
 
 _INTRA_NODE_PROCESS_GROUP, _INTER_NODE_PROCESS_GROUP = None, None
@@ -41,7 +70,8 @@ def get_local_world_size() -> int:
 
 def distributed_init(args):
     if any([x not in os.environ for x in ["RANK", "WORLD_SIZE", "MASTER_PORT", "MASTER_ADDR"]]):
-        _setup_dist_env_from_slurm(args)
+        # _setup_dist_env_from_slurm(args)
+        _setup_dist_env_from_launcher(args)
 
     dist.init_process_group("nccl")
     fs_init.initialize_model_parallel(args.model_parallel_size)
